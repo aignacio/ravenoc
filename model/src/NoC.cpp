941c859544c4154560ff72cc53529c998bce575a
Ânderson Ignacio da Silva <anderson.igns@gmail.com>
2020-07-23 15:25:46 +0100

    Removed the model

--- model/src/NoC.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * File              : NoC.cpp
- * Author            : Anderson Ignacio da Silva (aignacio) <anderson@aignacio.com>
- * Date              : 22.03.2020
- * Last Modified Date: 23.04.2020
- * Last Modified By  : Anderson Ignacio da Silva (aignacio) <anderson@aignacio.com>
- */
-#include "NoC.h"
