bf44a36b0432ef1911103b4e89510b1801184e1d
Anderson Ignacio da Silva <anderson@aignacio.com>
2021-03-03 22:09:34 +0000

    Fixed clock freq.

--- tb/default_values.py
+++ tb/default_values.py
@@ -3,7 +3,7 @@ import glob
 
 #@pytest.mark.skipif((os.getenv("SIM") != "verilator") and (os.getenv("SIM") != "xcelium") and (os.getenv("SIM") != "ius"), reason="Verilator/Xcelium are the only supported to simulate...")
 CLK_100MHz  = (10, "ns")
-CLK_200MHz  = (20, "ns")
+CLK_200MHz  = (5, "ns")
 RST_CYCLES  = 2
 tests_dir = os.path.dirname(os.path.abspath(__file__))
 rtl_dir   = os.path.join(tests_dir,"../src/")
