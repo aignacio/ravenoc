a83437f2b0abb775b891811529b392ed70a00a59
Anderson Ignacio da Silva <anderson@aignacio.com>
2021-06-23 22:31:10 +0100

    Fix for test_noc_csr due to code improvement
    Signed-off-by: Anderson Ignacio da Silva <anderson@aignacio.com>

--- tb/test_noc_csr.py
+++ tb/test_noc_csr.py
@@ -68,7 +68,8 @@ async def run_test(dut, config_clk="NoC_slwT_AXI", idle_inserter=None, backpress
     assert req.resp == AxiResp.OKAY, "AXI bus should have not raised an error here!"
     resp = await tb.read(sel=router, address=csr['IRQ_RD_MUX'], length=4, size=0x2)
     assert resp.resp == AxiResp.OKAY, "AXI bus should have raised an error here!"
-    data_in = int.from_bytes(rand_data, byteorder='little', signed=False)
+    # We need to do th e& 3 masking because this CSR has only 2-bits
+    data_in = int.from_bytes(rand_data, byteorder='little', signed=False) & 3
     data_out = int.from_bytes(resp.data, byteorder='little', signed=False)
     assert data_in == data_out, "NoC CSR, mismatch on IRQ_RD_MUX - Write/Read back"
 
